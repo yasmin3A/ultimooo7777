aea0e3fb380131293425908bb9d214e21ff2af63
yasmin3A <00001105948584sp@al.educacao.sp.gov.br>
2024-11-21 15:10:10 -0300

    Add files via upload

---
+++ flashcard-aula_10/assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o país mais populoso do mundo?',
+    ' Indía'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'Eu te amo em ingles I love you'
+)
---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o país mais populoso do mundo?',
+    ' O país mais poluso é a Indía'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilía'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Jesus eu te amo muito em Inglês?',
+    'Jesus eu te amo muito em ingles Jesus eu te amo muito'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu te amo em Inglês?',
+    'eu te amo em ingles I love you'
+)
